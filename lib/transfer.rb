86a90c959393698ce4d9197728be2705dcc57d6d
Mark A Guza <guzamaguza@gmail.com>
2020-02-08 22:59:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif @sender.balance < @amount 
+    elsif @sender.balance < @amount
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
