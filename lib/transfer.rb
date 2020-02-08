55589e289e70390920bd17793bf53911fc6237e1
Mark A Guza <guzamaguza@gmail.com>
2020-02-08 22:41:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,10 +12,10 @@ class Transfer
   end
 
   def execute_transaction
-    @sender.balance -= @amount 
-    @receiver.balance += @amount 
+    @sender.balance -= @amount
+    @receiver.balance += @amount
     @status = "complete"
   end
-  
+
 
 end
