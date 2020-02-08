33575630c9dbbff6dcccfe972b76906194e64cd3
Mark A Guza <guzamaguza@gmail.com>
2020-02-08 23:26:10 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-        if @sender.balance < @amount
+        if @sender.balance < @amount || @status != "pending"
           @status = "rejected"
           return "Transaction rejected. Please check your account balance."
         else
