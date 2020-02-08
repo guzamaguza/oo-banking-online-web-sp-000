20aae0770d346ddf57bf27fc45ab70ef7f4d1d34
Mark A Guza <guzamaguza@gmail.com>
2020-02-08 22:13:19 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
 class BankAccount
+  attr_accessor :name
+  attr_reader :status, :balance
+
+  def initialize(name)
+    @name = name.freeze
+    #@name.freeze
+    @status = "open"
+    @balance = 1000
+  end
+
+  def deposit(amount)
+    @balance = 1000 + amount
+    return @balance
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
+  def valid?
+    if @balance > 0 and @status == "open"
+      true
+    else
+      false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
 
 end
