e9a979f4435dfa0577cc1a82833494c02d23748a
Mark A Guza <guzamaguza@gmail.com>
2020-02-08 23:21:44 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,4 +1,4 @@
-=begin
+
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
 
@@ -34,40 +34,4 @@ class Transfer
     end
   end
 
-end
-=end
-
-require 'pry'
-class Transfer
-  attr_accessor :sender, :receiver, :amount, :status
-
-  def initialize(sender, receiver, amount)
-    @sender = sender
-    @receiver = receiver
-    @amount = amount
-    @status = "pending"
-  end
-
-  def valid?
-    @sender.valid? && @receiver.valid? ? true : false
-  end
-
-  def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance."
-    end
-  end
-
-  def reverse_transfer
-    if @status == "complete"
-      @sender.balance += @amount
-      @receiver.balance -= @amount
-      @status = "reversed"
-    end
-  end
 end
