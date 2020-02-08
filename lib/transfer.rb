b93ede8e6684d1888181084675fada9137489467
Mark A Guza <guzamaguza@gmail.com>
2020-02-08 22:20:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
 class Transfer
   # your code here
+  def initialize(sender, receiver)
+    @sender = sender 
+    @receiver = receiver 
+    @status = "pending"
+    @amount = 50 
+  end
+
+  def valid?
+    if @sender.status == "open" && @receiver.status == "open"
+
+    else
+
+    end
+    
+  end
+  
 end
