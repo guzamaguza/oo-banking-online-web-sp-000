842cb6380d5245dca243a3b77ca5ed149194f843
Mark A Guza <guzamaguza@gmail.com>
2020-02-08 23:46:45 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Transfer
   attr_accessor :sender, :receiver, :amount, :status
 
-  def initialize(reciever, sender, amount)
+  def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
     @status = "pending"
